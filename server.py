96de57614df418dfa972f6f3e70c8a2a31fc53b0
Muhammad Hamza Farrukh <farrukhhamza4@gmail.com>
2019-04-10 01:27:58 +0500

    binded the socket with the port and implemented listening

--- server.py
+++ server.py
@@ -16,3 +16,22 @@ def create_socket():
 
 	except socket.error as err:
 		print("socket creation error: " + str(err))
+
+#binding the socket and listening for connections
+
+def bind_socket():
+
+	try:
+		global host
+		global port
+		global s
+
+		print ("binding the Port: " + str(port))
+
+		s.bind(host,port)
+		s.listen(5) #number if connections it tolerates before throwing 
+
+	except socket.error as err:
+		print("socket binding error " + str(err) + "\n" + "Retrying...")
+		bind_socket()
+
